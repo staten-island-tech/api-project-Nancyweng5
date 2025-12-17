d9b288a0c249c8000626cc4a042c0973c9852208
Nancyweng5 <wengalex55@gmail.com>
2025-12-17 12:32:30 -0500

    .

--- genshin/src/main.js
+++ genshin/src/main.js
@@ -1,24 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+async function getData(genshin){
+    try{
+        //go get datat]
+        const response = await fetch(`https://genshin.jmp.blue/"${genshin}`);
+        //handle errors
+        if (response.status != 200){
+            throw new Error(response)
+        } else {
+            //makes the resoponse into json data we can use
+            const data = await response.json()
+            console.log(data);
+            document.getElementById("api-response").textConetnt = data.name;
+        }
+    } catch(error){
+        console.log(error);
+    }
+    }
+getData("characters/amber");
