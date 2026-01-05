d6084e2f75989f876e59bc3bb76f71b336caac57
Nancyweng5 <wengalex55@gmail.com>
2026-01-05 12:32:20 -0500

    .

--- genshin/src/main.js
+++ genshin/src/main.js
@@ -1,34 +1,38 @@
-const genshin = "`https://genshin.jmp.blue/"
-function inject(items){
+import "./style.css";
+const genshin = "https://genshin.jmp.blue/characters"
+function inject(characters){
     const genshindata = document.querySelector(".api-response");
     genshindata.insertAdjacentHTML("beforeend",
-        `<div class = genshindata>
-            <h3>Name: ${items.name}</h3>
-            <h3>Title: ${items.title}</h3>
-            <h3>Vision: ${items.vision}</h3>
-            <h3>Weapon: ${items.weapon}</h3>
-            <h3>Gender: ${items.gender}</h3>
-            <h3>Nation : ${items.nation}</h3>
-            <h3>Affiliation: ${items.affiliation}</h3>
-            <h3>Rarity: ${items.rarity}</h3>
+        `<div class = "genshindata">
+            <h3>Name: ${characters.name}</h3>
+            <h3>Title: ${characters.title}</h3>
+            <h3>Vision: ${characters.vision}</h3>
+            <h3>Weapon: ${characters.weapon}</h3>
+            <h3>Gender: ${characters.gender}</h3>
+            <h3>Nation : ${characters.nation}</h3>
+            <h3>Affiliation: ${characters.affiliation}</h3>
+            <h3>Rarity: ${characters.rarity}</h3>
         </div>`
     )
-}
-async functi         on getData(genshin){
+ }
+
+async function getData(genshin){
     try{
-        //go get data]
         const response = await fetch(genshin);
-        //handle errors
         if (response.status != 200){
             throw new Error(response)
         } else {
-            //makes the resoponse into json data we can use
             const data = await response.json()
             console.log(data);
-            data.entries.forEach(inject)
+            data.entries.forEach(inject);
         }
     } catch(error){
         console.log(error);
     }
     }
 getData(genshin);
+
+/* function searchbar({
+    
+})
+ */
